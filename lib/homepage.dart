28d700fdccd08bb0b12c611df729d14266382cd1
HP Computer s <sohailokzz@gmail.com>
2022-11-05 12:16:45 +0500

    HomePage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:upwork_demo/chat_view.dart';
-import 'package:upwork_demo/data/all_contacts.dart';
+import 'package:upwork_demo/chat_room_page.dart';
+import 'package:upwork_demo/models/chat_room_model.dart';
+import 'package:upwork_demo/models/firebase_helper.dart';
 import 'package:upwork_demo/models/user_model.dart';
 import 'package:upwork_demo/phone.dart';
 import 'package:upwork_demo/search_page.dart';
@@ -46,6 +48,91 @@ class _MyHomeState extends State<MyHome> {
           ),
         ],
       ),
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("chatRoom")
+              .where("participant.${widget.userModel.uid}", isEqualTo: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasData) {
+                QuerySnapshot chatRoomSnapshot = snapshot.data as QuerySnapshot;
+                return ListView.builder(
+                  itemCount: chatRoomSnapshot.docs.length,
+                  itemBuilder: (context, index) {
+                    ChatRoomModel chatRoomModel = ChatRoomModel.fromMap(
+                      chatRoomSnapshot.docs[index].data()
+                          as Map<String, dynamic>,
+                    );
+                    Map<String, dynamic> participants =
+                        chatRoomModel.participant!;
+                    List<String> participantKey = participants.keys.toList();
+                    participantKey.remove(widget.userModel.uid);
+
+                    return FutureBuilder(
+                      future: FirebaseHelper.getUserModel(participantKey[0]),
+                      builder: (context, userData) {
+                        if (userData.connectionState == ConnectionState.done) {
+                          if (userData.data != null) {
+                            UserModel targetUser = userData.data as UserModel;
+                            return ListTile(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return ChatRoomPage(
+                                        targetUser: targetUser,
+                                        chatRoom: chatRoomModel,
+                                        userModel: widget.userModel,
+                                        firebaseUser: widget.firebaseUser,
+                                      );
+                                    },
+                                  ),
+                                );
+                              },
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  targetUser.profileImage.toString(),
+                                ),
+                              ),
+                              title: Text(
+                                targetUser.fullName.toString(),
+                              ),
+                              subtitle: Text(
+                                chatRoomModel.lastMessage.toString(),
+                              ),
+                            );
+                          } else {
+                            return Container();
+                          }
+                        } else {
+                          return Container();
+                        }
+                      },
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    snapshot.error.toString(),
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: Text('No Chats'),
+                );
+              }
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -62,95 +149,6 @@ class _MyHomeState extends State<MyHome> {
         },
         child: const Icon(
           Icons.search,
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: allContacts.length,
-        itemBuilder: (context, index) {
-          return buildAllContacts(
-            context,
-            index,
-          );
-        },
-      ),
-    );
-  }
-
-  Widget buildAllContacts(
-    BuildContext context,
-    int index,
-  ) {
-    Map allContactList = allContacts[index];
-    return ListTile(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ChatView(
-              title: allContactList["title"],
-              image: allContactList["image"],
-            ),
-          ),
-        );
-      },
-      leading: Container(
-        height: 52,
-        width: 52,
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.blue,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(50),
-          child: Image.asset(
-            allContactList["image"],
-          ),
-        ),
-      ),
-      title: Text(
-        allContactList["title"],
-      ),
-      subtitle: Text(
-        allContactList["message"],
-      ),
-    );
-  }
-
-  Widget allContactDesigns(
-    BuildContext context,
-    int index,
-  ) {
-    Map allContactList = allContacts[index];
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        margin: const EdgeInsets.only(top: 12),
-        width: MediaQuery.of(context).size.width,
-        height: 280,
-        decoration: BoxDecoration(
-          color: const Color(0XFFD6F0F5),
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: const Color(0xFF00CEE3),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 12,
-              ),
-              child: Text(
-                allContactList["title"],
-              ),
-            ),
-            Center(
-              child: Image.asset(
-                allContactList["image"],
-              ),
-            )
-          ],
         ),
       ),
     );
